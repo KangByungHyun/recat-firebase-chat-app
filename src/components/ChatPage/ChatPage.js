b6950cbb3a09b3fa58e6cbbb2ff1a78c79ec6e0f
KangByungHyun <justone702@gmail.com>
2022-05-26 22:04:42 +0900

    채팅 UI

--- src/components/ChatPage/ChatPage.js
+++ src/components/ChatPage/ChatPage.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import SidePanel from './SidePanel/SidePanel'
+import MainPanel from './MainPanel/MainPanel'
 
 function ChatPage() {
   return (
-    <div>
-        ChatPage
+    <div style={{ display: 'flex' }}>
+      <div style={{ width: '300px' }}>
+          <SidePanel
+              //key={currentUser && currentUser.uid}
+          />
+      </div>
+      <div style={{ width: '100%' }}>
+          <MainPanel
+              //key={currentChatRoom && currentChatRoom.id}
+          />
+      </div>
     </div>
   )
 }
---
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChatRomms() {
+  return (
+    <div>ChatRomms</div>
+  )
+}
+
+export default ChatRomms
